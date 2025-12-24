03996cc85204d05ffd41241d9d674971eff001ce
dif737 <ayoubdif0@gmail.com>
2025-12-24 23:20:43 +0100

    TP 5 + 6 React Native - Application unifiée

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import AuthProvider, { AuthContext } from './context/AuthContext';
+import AppDrawer from './navigation/AppDrawer';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen name="Accueil" component={HomeScreen} />
+      <Tab.Screen name="Tâches" component={AppDrawer} />
+      <Tab.Screen name="Paramètres" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function RootNavigator() {
+  const { user } = useContext(AuthContext);
+  return user ? <TabNavigator /> : <LoginScreen />;
+}
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </AuthProvider>
+    </Provider>
+  );
+}
